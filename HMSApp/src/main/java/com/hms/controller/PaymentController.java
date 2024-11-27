5ba65afe8dab6d6234a160045d3975c1ab261563
Deekshita31 <21b01a0581@svecw.edu.in>
2024-11-27 19:55:33 +0530

    added controller by Lakshmi

---
+++ HMSApp/src/main/java/com/hms/controller/PaymentController.java
@@ -0,0 +1,21 @@
+package com.hms.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.hms.entities.Payment;
+//import com.hms.service.PaymentService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/payments")
+public class PaymentController {
+
+    //@Autowired
+    //private PaymentService service;
+
+  }
+ 
--- HMSApp/src/main/java/com/hms/entities/Doctor.java
+++ HMSApp/src/main/java/com/hms/entities/Doctor.java
@@ -5,6 +5,72 @@ import java.time.LocalDate;
 public class Doctor {
 
 	int doctorId;
+	public int getDoctorId() {
+		return doctorId;
+	}
+	public void setDoctorId(int doctorId) {
+		this.doctorId = doctorId;
+	}
+	public String getDoctorName() {
+		return doctorName;
+	}
+	public void setDoctorName(String doctorName) {
+		this.doctorName = doctorName;
+	}
+	public String getSpecialization() {
+		return specialization;
+	}
+	public void setSpecialization(String specialization) {
+		this.specialization = specialization;
+	}
+	public String getQualification() {
+		return qualification;
+	}
+	public void setQualification(String qualification) {
+		this.qualification = qualification;
+	}
+	public String getContactNumber() {
+		return contactNumber;
+	}
+	public void setContactNumber(String contactNumber) {
+		this.contactNumber = contactNumber;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	public String getLocation() {
+		return Location;
+	}
+	public void setLocation(String location) {
+		Location = location;
+	}
+	public LocalDate getDateofJoining() {
+		return dateofJoining;
+	}
+	public void setDateofJoining(LocalDate dateofJoining) {
+		this.dateofJoining = dateofJoining;
+	}
+	public boolean isSurgeon() {
+		return isSurgeon;
+	}
+	public void setSurgeon(boolean isSurgeon) {
+		this.isSurgeon = isSurgeon;
+	}
+	public int getYearsOfExperience() {
+		return yearsOfExperience;
+	}
+	public void setYearsOfExperience(int yearsOfExperience) {
+		this.yearsOfExperience = yearsOfExperience;
+	}
+	public boolean isStatus() {
+		return status;
+	}
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
 	String doctorName;
 	String specialization;
 	String qualification;
