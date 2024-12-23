d06aa1d828c8a0d631655d2f6624be3d093ca573
VigneshVinayan <vignesh04kv@gmail.com>
2024-12-23 16:49:18 +0530

    Doctor UI

--- HMSAppUI/src/main/java/com/hms/entities/Doctor.java
+++ HMSAppUI/src/main/java/com/hms/entities/Doctor.java
@@ -1,5 +1,145 @@
-package com.hms.entities;
+package com.hmsui.model;
+
+import java.time.LocalDate;
+
 
 public class Doctor {
 
+   
+
+    private int doctorId;
+
+    private String doctorName;
+
+    private String specialization;
+    private String qualification;
+
+    private String contactNumber;
+
+    private String emailId;
+
+    private String gender;
+
+    private String location;
+
+    private double consultationFees;
+
+    private LocalDate dateOfJoining;
+
+    private boolean Surgeon;
+
+    private int yearsOfExperience;
+    private boolean status=true;
+
+
+    public int getDoctorId() {
+        return doctorId;
+    }
+
+    public void setDoctorId(int doctorId) {
+        this.doctorId = doctorId;
+    }
+
+    public String getDoctorName() {
+        return doctorName;
+    }
+
+    public void setDoctorName(String doctorName) {
+        this.doctorName = doctorName;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
+    public String getQualification() {
+        return qualification;
+    }
+
+    public void setQualification(String qualification) {
+        this.qualification = qualification;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public double getConsultationFees() {
+        return consultationFees;
+    }
+
+    public void setConsultationFees(double consultationFees) {
+        this.consultationFees = consultationFees;
+    }
+
+    public LocalDate getDateOfJoining() {
+        return dateOfJoining;
+    }
+
+    public void setDateOfJoining(LocalDate dateOfJoining) {
+        this.dateOfJoining = dateOfJoining;
+    }
+
+    public boolean isSurgeon() {
+        return Surgeon;
+    }
+
+    public void setSurgeon(boolean Surgeon) {
+        this.Surgeon = Surgeon;
+    }
+
+    public int getYearsOfExperience() {
+        return yearsOfExperience;
+    }
+
+    public void setYearsOfExperience(int yearsOfExperience) {
+        this.yearsOfExperience = yearsOfExperience;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+	public int getId() {
+		// TODO Auto-generated method stub
+		return doctorId;
+	}
 }
+
+ 
