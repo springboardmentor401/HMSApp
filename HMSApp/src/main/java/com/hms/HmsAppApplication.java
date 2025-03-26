c0b3dd301ccb5502af6f6dea541d0ec077cd4387
springboardmentor401 <springboardmentor401@gmail.com>
2025-03-26 16:12:22 +0530

    just for test added a print

--- HMSApp/src/main/java/com/hms/HmsAppApplication.java
+++ HMSApp/src/main/java/com/hms/HmsAppApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HmsAppApplication {
 
 	public static void main(String[] args) {
+		System.out.println("xxx");
 		SpringApplication.run(HmsAppApplication.class, args);
 	}
 
